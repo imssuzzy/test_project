88cf9387bd236bcb1c9a7a4f6745ebb5a756fe25
imssuzzy <sezim.cholponkulova@gmail.com>
2024-08-18 12:18:14 +0600

    change mosels

--- house_project/house_project/models.py
+++ house_project/house_project/models.py
@@ -11,7 +11,7 @@ class Property(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, related_name='properties', on_delete = models.CASCADE)
+    category = models.ForeignKey(Category, related_name='categories', on_delete = models.CASCADE)
 
     def __str__(self):
         return self.name
